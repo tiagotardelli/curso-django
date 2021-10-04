edd42ec849a28d47626012fa7b1a28fecb6fe316
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-10-03 21:26:06 -0300

    Criada app base close #16

--- pypro/settings.py
+++ pypro/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pypro.base',
 ]
 
 MIDDLEWARE = [
