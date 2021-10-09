475d5bd6fe68829bb93ce6f139206cb889f72e29
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-10-09 06:44:04 -0300

    Alterado o settings para o BD close #26

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+import dj_database_url
 
+from functools import partial
 from pathlib import Path
 from decouple import config, Csv
 
@@ -74,12 +77,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 
