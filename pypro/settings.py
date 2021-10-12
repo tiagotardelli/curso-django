f188eb8cda4078652a3d0d15674e01a0c5512317
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-10-11 23:14:46 -0300

    Configurando arquivos estáticos e media close #34

--- pypro/settings.py
+++ pypro/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# Configuração de ambiente de desenvolvimento
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
